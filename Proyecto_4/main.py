cc9bb26e2c79151aafddd4f94e772c62b4e8cf8c
Fernando <ruferesfer@gmail.com>
2025-11-01 19:50:05 -0600

    updates esteticos

--- Proyecto_4/main.py
+++ Proyecto_4/main.py
@@ -1,10 +1,9 @@
-# main.py
-import sys
 from PyQt6.QtWidgets import QApplication
 from crypto_Ui import CryptoApp
+import sys
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    win = CryptoApp()
-    win.show()
+    window = CryptoApp()
+    window.show()
     sys.exit(app.exec())
